ae22dd0aa3800d57996b8596c3ad4532af9f5fee
Marcel Admiraal <madmiraal@users.noreply.github.com>
2025-07-19 15:07:21 +0200

    Migrate to Gradle Java toolchains

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,14 +25,11 @@ android {
             )
         }
     }
+}
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
     }
 }
 
